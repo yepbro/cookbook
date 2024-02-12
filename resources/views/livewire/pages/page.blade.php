c54c2a776caeea23ae037452006f6863c5d71861
Denis <denis@yepbro.ru>
2024-02-12 12:38:28 +0300

    Текстовая страница

--- resources/views/livewire/pages/page.blade.php
+++ resources/views/livewire/pages/page.blade.php
@@ -1,5 +1,24 @@
 <div>
     <div class="pt-16 lg:pt-20">
-    {!! $page->content !!}
+
+        <div class="prose prose max-w-none border-b border-grey-lighter pb-8 dark:prose-dark sm:pb-12">
+            {{ $page->content }}
+        </div>
+
+        <div class="flex items-center pt-16 lg:pt-20">
+            <span class="pr-5 font-body font-medium text-primary dark:text-white">Share</span>
+            <a href="/">
+                <i class="bx bxl-facebook text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
+            </a>
+            <a href="/">
+                <i class="bx bxl-twitter pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
+            </a>
+            <a href="/">
+                <i class="bx bxl-linkedin pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
+            </a>
+            <a href="/">
+                <i class="bx bxl-reddit pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
+            </a>
+        </div>
     </div>
 </div>
