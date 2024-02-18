7dfe8ccc4280e9193fa39aeed55e44077490662f
Denis <denis@yepbro.ru>
2024-02-18 18:00:19 +0300

    Убран блок share

--- resources/views/livewire/pages/articles/show.blade.php
+++ resources/views/livewire/pages/articles/show.blade.php
@@ -51,20 +51,5 @@ const x = () => 5;
             </div>
         @endif
 
-        <div class="flex items-center pt-16 lg:pt-20">
-            <span class="pr-5 font-body font-medium text-primary dark:text-white">Share</span>
-            <a href="/">
-                <i class="bx bxl-facebook text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
-            </a>
-            <a href="/">
-                <i class="bx bxl-twitter pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
-            </a>
-            <a href="/">
-                <i class="bx bxl-linkedin pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
-            </a>
-            <a href="/">
-                <i class="bx bxl-reddit pl-2 text-2xl text-primary transition-colors hover:text-secondary dark:text-white dark:hover:text-secondary"></i>
-            </a>
-        </div>
     </div>
 </div>
