8712f0100ec9906150d933068e653136308e5876
Denis <denis@yepbro.ru>
2024-02-23 21:29:36 +0300

    Правка оформления тегов

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -4,8 +4,9 @@
  */
 ?>
 @foreach($tags as $tag)
-    <span class="mb-4 inline-block rounded-full px-2 py-1 font-body text-sm text-slate-800 {{ $tag->getClass() }}">
-        <a href="{{ route('pages.tags.show', $tag) }}">
+    <span class="mb-4 inline-block rounded-full px-2 py-1 font-body text-sm {{ $tag->getClass() }}">
+        <a href="{{ route('pages.tags.show', $tag) }}"
+           class="no-underline text-slate-800 hover:text-slate-800 visited:text-slate-800">
             {{ $tag->name }}
         </a>
     </span>
