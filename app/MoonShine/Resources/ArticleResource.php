0cace0a0c14b3002f5c770ebc2323555b6a61e94
Denis <denis@yepbro.ru>
2024-03-20 17:52:17 +0300

    Раздел Админки для тегов - список статей

--- app/MoonShine/Resources/ArticleResource.php
+++ app/MoonShine/Resources/ArticleResource.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace App\MoonShine\Resources;
 
 use App\Models\Article;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use MoonShine\Fields\Date;
 use MoonShine\Fields\ID;
+use MoonShine\Fields\Preview;
+use MoonShine\Fields\Relationships\BelongsToMany;
+use MoonShine\Fields\Text;
+use MoonShine\QueryTags\QueryTag;
 use MoonShine\Resources\ModelResource;
 
 /**
@@ -18,10 +24,36 @@ class ArticleResource extends ModelResource
 
     protected string $title = 'Статьи';
 
+    protected array $with = [
+        'author',
+    ];
+
+    public function queryTags(): array
+    {
+        return [
+            QueryTag::make(
+                'Активные',
+                fn (Builder $query): Builder => $query->withoutTrashed()
+            )->icon('heroicons.power')
+                ->default(),
+            QueryTag::make(
+                'Удаленные',
+                fn (Builder $query): Builder => $query->onlyTrashed()
+            )->icon('heroicons.trash'),
+        ];
+    }
+
     public function indexFields(): array
     {
         return [
             ID::make()->sortable(),
+            Text::make('Автор', 'author.name'),
+            Text::make('Название', 'heading'),
+            Preview::make('Опубликовано', 'is_published')->boolean(),
+            BelongsToMany::make('Теги', 'tags', resource: new TagResource)
+                ->inLine(' ', true),
+            Date::make('Добавлено', 'created_at')
+                ->format('d.m.Y'),
         ];
     }
 
